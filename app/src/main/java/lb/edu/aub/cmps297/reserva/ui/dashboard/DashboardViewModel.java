fb8746f4274306601ef555eef3c696f5fa2374eb
Omar Karazoun <omar.kar101@gmail.com>
2021-10-23 19:22:53 +0300

    initialize repository

---
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package lb.edu.aub.cmps297.reserva.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
