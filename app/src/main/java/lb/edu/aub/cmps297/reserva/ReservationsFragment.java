43d72e530f28e54740ae9c8cd227079f50837f25
Omar Karazoun <omar.kar101@gmail.com>
2021-11-28 17:15:15 +0200

    .

--- app/src/main/java/lb/edu/aub/cmps297/reserva/ReservationsFragment.java
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/ReservationsFragment.java
@@ -13,11 +13,13 @@ import android.view.ViewGroup;
 import lb.edu.aub.cmps297.reserva.databinding.FragmentProfileBinding;
 import lb.edu.aub.cmps297.reserva.databinding.FragmentReservationsBinding;
 import lb.edu.aub.cmps297.reserva.views.RestaurantAdapter;
+import lb.edu.aub.cmps297.reserva.views.RestaurantCurrentReservationAdapter;
 import lb.edu.aub.cmps297.reserva.views.RestaurantIncomingRequestsAdapter;
 
 public class ReservationsFragment extends Fragment {
     private FragmentReservationsBinding binding;
     private RecyclerView incomingRequestsRV;
+    private RecyclerView currentReservationsRV;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,11 +27,20 @@ public class ReservationsFragment extends Fragment {
         binding = FragmentReservationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         incomingRequestsRV = root.findViewById(R.id.idRVRestaurantIncomingRequests);
+//        currentReservationsRV = root.findViewById(R.id.idRVRestaurantCurrentReservations);
+
 
         RestaurantIncomingRequestsAdapter incomingRequestsAdapter = new RestaurantIncomingRequestsAdapter(this.getContext(), StaticStorage.restaurants);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
-        incomingRequestsRV.setLayoutManager(linearLayoutManager);
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+        incomingRequestsRV.setLayoutManager(linearLayoutManager1);
         incomingRequestsRV.setAdapter(incomingRequestsAdapter);
+
+
+
+//        RestaurantCurrentReservationAdapter currentReservationAdapter = new RestaurantCurrentReservationAdapter(this.getContext(),StaticStorage.restaurants);
+//        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+//        currentReservationsRV.setLayoutManager(linearLayoutManager2);
+//        currentReservationsRV.setAdapter(currentReservationAdapter);
         return root;
     }
 
