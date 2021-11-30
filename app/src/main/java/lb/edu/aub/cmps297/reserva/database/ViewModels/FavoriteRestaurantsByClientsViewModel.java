f00a3b96267ca58278f7bb8a7ee6d4f5454bfee0
Omar Karazoun <omar.kar101@gmail.com>
2021-11-30 12:47:16 +0200

    .

---
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/database/ViewModels/FavoriteRestaurantsByClientsViewModel.java
@@ -0,0 +1,31 @@
+package lb.edu.aub.cmps297.reserva.database.ViewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import java.util.List;
+
+import lb.edu.aub.cmps297.reserva.database.Entities.Restaurant;
+import lb.edu.aub.cmps297.reserva.database.Repositories.FavoriteRestaurantsByClientsRepository;
+
+public class FavoriteRestaurantsByClientsViewModel extends AndroidViewModel {
+    private final FavoriteRestaurantsByClientsRepository favoriteRestaurantsByClientsRepository;
+    public FavoriteRestaurantsByClientsViewModel(@NonNull Application application) {
+        super(application);
+        favoriteRestaurantsByClientsRepository = new FavoriteRestaurantsByClientsRepository(application);
+    }
+
+    public List<Restaurant> getAllFavoriteRestaurants(String client_email) {
+        return favoriteRestaurantsByClientsRepository.getAllFavoriteRestaurants(client_email);
+    }
+
+    public void removeFavoriteRestaurant(String client_email, String restaurant_email) {
+        favoriteRestaurantsByClientsRepository.removeFavoriteRestaurant(client_email, restaurant_email);
+    }
+
+    public void addFavoriteRestaurant(String client_email, String restaurant_email) {
+        favoriteRestaurantsByClientsRepository.addFavoriteRestaurant(client_email, restaurant_email);
+    }
+}
