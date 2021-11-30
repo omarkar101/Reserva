ce0664c472dc493ac16bf81e48e1b249f2ead634
Ali Srour <73133529+SrourHub@users.noreply.github.com>
2021-11-30 19:37:54 +0200

    fixed the button

--- app/src/main/java/lb/edu/aub/cmps297/reserva/ReservationsFragment.java
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/ReservationsFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import lb.edu.aub.cmps297.reserva.adapters.RestaurantAdapter;
+import lb.edu.aub.cmps297.reserva.database.Entities.LoggedInUser;
+import lb.edu.aub.cmps297.reserva.database.Entities.Restaurant;
+import lb.edu.aub.cmps297.reserva.database.ViewModels.LoggedInUserViewModel;
+import lb.edu.aub.cmps297.reserva.database.ViewModels.RestaurantViewModel;
 import lb.edu.aub.cmps297.reserva.databinding.FragmentReservationsBinding;
 import lb.edu.aub.cmps297.reserva.adapters.RestaurantCurrentReservationAdapter;
 import lb.edu.aub.cmps297.reserva.adapters.RestaurantIncomingRequestsAdapter;
@@ -20,6 +26,13 @@ public class ReservationsFragment extends Fragment {
     private FragmentReservationsBinding binding;
     private RecyclerView incomingRequestsRV;
     private RecyclerView currentReservationsRV;
+    private Button seatsRemaining;
+
+    private Restaurant restaurant;
+
+    private LoggedInUserViewModel loggedInUserViewModel;
+
+    private RestaurantViewModel restaurantViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,7 +40,15 @@ public class ReservationsFragment extends Fragment {
         View root = binding.getRoot();
         incomingRequestsRV = root.findViewById(R.id.idRVRestaurantIncomingRequests);
         currentReservationsRV = root.findViewById(R.id.idRVRestaurantCurrentReservations);
+        seatsRemaining = root.findViewById(R.id.idRestaurantReservationsSeatsRemainingText);
 
+        loggedInUserViewModel = new ViewModelProvider(this).get(LoggedInUserViewModel.class);
+        restaurantViewModel = new ViewModelProvider(this).get(RestaurantViewModel.class);
+        LoggedInUser loggedInUser = loggedInUserViewModel.getUser();
+
+        restaurant = restaurantViewModel.getRestaurant(loggedInUser.email);
+
+        seatsRemaining.setText("Seats Remaining: "+Integer.valueOf(restaurant.seatsMaxCapacity - restaurant.seatsAvailable).toString());
 
         RestaurantIncomingRequestsAdapter incomingRequestsAdapter = new RestaurantIncomingRequestsAdapter(this.getContext(), StaticStorage.restaurants);
         LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
