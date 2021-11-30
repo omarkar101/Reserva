d11e04f3064e962ccde2f09141ca002661b052e3
Omar Karazoun <omar.kar101@gmail.com>
2021-11-30 12:51:06 +0200

    .

--- app/src/main/java/lb/edu/aub/cmps297/reserva/ui/home/HomeFragment.java
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/ui/home/HomeFragment.java
@@ -8,12 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lb.edu.aub.cmps297.reserva.R;
 import lb.edu.aub.cmps297.reserva.StaticStorage;
+import lb.edu.aub.cmps297.reserva.database.Entities.Restaurant;
+import lb.edu.aub.cmps297.reserva.database.ViewModels.RestaurantViewModel;
 import lb.edu.aub.cmps297.reserva.databinding.FragmentHomeBinding;
 import lb.edu.aub.cmps297.reserva.ui.favorites.FavoritesFragment;
 import lb.edu.aub.cmps297.reserva.views.RestaurantAdapter;
@@ -22,17 +28,21 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private RecyclerView restaurantRV;
+    private RestaurantViewModel restaurantViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         restaurantRV = root.findViewById(R.id.idRVRestaurant);
-        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(this.getContext(), StaticStorage.restaurants);
+        restaurantViewModel = new ViewModelProvider(this).get(RestaurantViewModel.class);
+
+        List<Restaurant> restaurants = restaurantViewModel.getAll();
+
+        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(this.getContext(), (ArrayList<Restaurant>) restaurants);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
         restaurantRV.setLayoutManager(linearLayoutManager);
         restaurantRV.setAdapter(restaurantAdapter);
-
 
         return root;
     }
