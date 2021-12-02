305e67d066061c34bdc7a61870a7c749ff6a2aeb
Omar Karazoun <omar.kar101@gmail.com>
2021-12-02 13:47:59 +0200

    add toast to save changes

--- app/src/main/java/lb/edu/aub/cmps297/reserva/RestaurantHomeFragment.java
+++ app/src/main/java/lb/edu/aub/cmps297/reserva/RestaurantHomeFragment.java
@@ -1,8 +1,5 @@
 package lb.edu.aub.cmps297.reserva;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,9 +12,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 import lb.edu.aub.cmps297.reserva.database.Entities.LoggedInUser;
@@ -40,7 +36,6 @@ public class RestaurantHomeFragment extends Fragment {
     private ImageButton restaurantArrowUp;
     private ImageButton restaurantArrowDown;
     private Button restaurantSaveChanges;
-//    private Button restaurantEditInfoBtn;
 
     private LoggedInUserViewModel loggedInUserViewModel;
 
@@ -52,8 +47,6 @@ public class RestaurantHomeFragment extends Fragment {
         binding = FragmentRestaurantHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
-//        hek men jeeb el sura men database mafrud
 
 
         ArrayList<Integer> menuImgs = new ArrayList<Integer>();
@@ -69,7 +62,6 @@ public class RestaurantHomeFragment extends Fragment {
         restaurant = restaurantViewModel.getRestaurant(loggedInUser.email);
         if(restaurant == null) return root;
 
-//        restaurant = new Restaurant("Food stop", "37214721", 100, "dfji23jfdoui3wenoc", "wmjenfnwe", R.drawable.ic_dashboard_black_24dp, menu);
         restaurantImg = root.findViewById(R.id.idRestarauntHomeImg);
         restaurantName = root.findViewById(R.id.idRestaurantHomeName);
         restaurantDescriptionText = root.findViewById(R.id.idRestaurantHomeDescriptionText);
@@ -79,7 +71,6 @@ public class RestaurantHomeFragment extends Fragment {
         restaurantArrowUp = root.findViewById(R.id.idRestaurantHomeArrowUpBtn);
         restaurantArrowDown = root.findViewById(R.id.idRestaurantHomeArrowDownBtn);
         restaurantSaveChanges = root.findViewById(R.id.idRestaurantHomeSaveChangesBtn);
-//        restaurantEditInfoBtn = root.findViewById(R.id.idRestaurantHomeEditInfo);
 
         restaurantImg.setImageResource(R.drawable.ic_dashboard_black_24dp);
         restaurantName.setText(restaurant.name);
@@ -88,13 +79,12 @@ public class RestaurantHomeFragment extends Fragment {
         restaurantLocationText.setText(restaurant.location);
         restaurantSeatsNumber.setText(Integer.valueOf(restaurant.seatsMaxCapacity).toString());
 
-//        restaurantEditInfoBtn.setOnClickListener(this);
-        
 
 
-        restaurantSaveChanges.setOnClickListener(view ->
-                restaurantViewModel.updateRestaurantSeatsNumber(restaurant.email,Integer.parseInt(restaurantSeatsNumber.getText().toString()))
-        );
+        restaurantSaveChanges.setOnClickListener(view -> {
+            restaurantViewModel.updateRestaurantMaxSeatsNumber(restaurant.email, Integer.parseInt(restaurantSeatsNumber.getText().toString()));
+            Toast.makeText(RestaurantHomeFragment.this.getContext(), "Saved Changes!", Toast.LENGTH_LONG).show();
+        });
         restaurantArrowUp.setOnClickListener(view -> {
             Integer count = Integer.parseInt(restaurantSeatsNumber.getText().toString());
             count++;
@@ -115,10 +105,4 @@ public class RestaurantHomeFragment extends Fragment {
 
         return root;
     }
-
-//    @Override
-//    public void onClick(View v) {
-//        Intent intent = new Intent(this.getContext(), RestaurantEditInfo.class);
-//        startActivity(intent);
-//    }
 }
